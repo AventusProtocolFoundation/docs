f04b8f00eb7c79da4cf93eff0be0d68a6503974b
ethan <ethan.illingworth@artos.io>
2019-09-16 14:15:51 +0100

    whitepaper menu bar link removed

--- website/siteConfig.js
+++ website/siteConfig.js
@@ -37,8 +37,7 @@ const siteConfig = {
 
   // For no header links in the top nav bar -> headerLinks: [],
   headerLinks: [
-    { href: 'https://github.com/AventusProtocolFoundation', label: 'GitHub' },
-    { href: 'https://github.com/AventusProtocolFoundation/docs/blob/master/resources/Aventus%20Classic%20Whitepaper.pdf', label: 'Whitepaper' },
+    { href: 'https://github.com/AventusProtocolFoundation', label: 'GitHub' }
   ],
 
   // If you have users set above, you add it here:
